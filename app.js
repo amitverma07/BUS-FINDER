ab85ab6638c53f00436a8945c603d6a61f61f0a7
amitverma07 <theverma7@gmail.com>
2019-12-17 14:35:15 -0600

    Adds styles to make HTML better

---
+++ app.js
@@ -0,0 +1,20 @@
+const henlow = document.querySelector('#henlowBay');
+const aldgate = document.querySelector('#aldgateRoad');
+
+aldgate.addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log('hi');
+  //console.log(searchForm.nodeValue);
+
+  //const busapi = new XMLHttpRequest();
+
+})
+
+henlow.addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log('hello');
+  //console.log(searchForm.nodeValue);
+
+  //const busapi = new XMLHttpRequest();
+
+})
